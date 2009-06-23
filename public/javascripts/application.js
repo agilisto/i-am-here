964a55be34edb6d82047c5a4117f624409925fe9
hendrikl <hendrikl@foundation-1.local>
2009-06-23 11:52:10 +0200

    fixed save functionality with some jquery-fu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+    $('#save-me-button').click(function() {
+        if( $('#more_information').css("display") == "none" ){
+            $('#more_information').grow();
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
+});
